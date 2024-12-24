a1b0ec60d27881075c683572794504ee3c272636
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2024-12-24 10:30:16 -0500

    completes day 6 of advent of code 2024

--- 2024/day6/script.py
+++ 2024/day6/script.py
@@ -1,4 +1,4 @@
-
+# from pprint import pprint
 path = "."
 block = "#"
 up = "^"
@@ -10,7 +10,7 @@ direction_pointer = {up: right, right: down, down: left, left: up}
 
 
 def process_input(path):
-    global directions
+
     # we want to return a list of containing strings for easy iterations
     with open(path) as file:
         content = file.readlines()
@@ -30,8 +30,6 @@ lab, guard_direction, guard_position = process_input("input.txt")
 
 
 def simulate_guard(lab, guard_direction, guard_position):
-    global directions
-    global direction_pointer
     rows = len(lab)
     cols = len(lab[0])
     visited_positions = set()
@@ -56,3 +54,49 @@ def simulate_guard(lab, guard_direction, guard_position):
 
 
 print(simulate_guard(lab, guard_direction, guard_position))
+
+
+# Part 2
+
+def detect_loop(lab, guard_direction, guard_position):
+    rows = len(lab)
+    cols = len(lab[0])
+    row, col = guard_position
+    loop_found = False
+    exits = False
+    direction = guard_direction
+    visited = {(row, col, direction): 1}
+    while not loop_found and not exits:
+        d_row, d_col = directions[direction]
+        row, col = row + d_row, col + d_col
+        if 0 <= row < rows and 0 <= col < cols and lab[row][col] != block:
+            if not ((row, col, direction) in visited):
+                visited[(row, col, direction)] = 1
+            else:
+                return True
+        elif 0 <= row < rows and 0 <= col < cols and lab[row][col] == block:
+            row, col = row - d_row, col - d_col
+            direction = direction_pointer[direction]
+        if row < 0 or row >= rows or col < 0 or col >= cols:
+            exits = True
+
+    return False
+
+
+def find_possible_loops(lab, guard_direction, guard_position):
+    loops = 0
+    new_lab = lab[:]
+    for row in range(len(lab)):
+        for col in range(len(lab[row])):
+            if lab[row][col] == path:
+                new_lab[row] = lab[row][:col] + "#" + lab[row][col + 1:]
+                # pprint(new_lab)
+                loop_found = detect_loop(
+                    new_lab, guard_direction, guard_position)
+                if loop_found:
+                    loops += 1
+            new_lab = lab[:]
+    return loops
+
+
+print(find_possible_loops(lab, guard_direction, guard_position))
