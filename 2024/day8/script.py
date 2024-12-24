872d43cd2d1a8dfc175e320385ccddd34a17e1dc
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2024-12-24 17:14:25 -0500

    removes txt files and adds day8 part 1

---
+++ 2024/day8/script.py
@@ -0,0 +1,54 @@
+# --- Day 8: Resonant Collinearity ---
+
+# Antenas with the same frequency
+# one twice as far as as the other
+# any pair of antenas with the same frequency, there are two
+# antinodes; one on each side.
+# Antinodes can occur locations that contain antenas
+
+
+def process_input(path):
+    with open(path) as file:
+        content = file.readlines()
+    content = [row.strip("\n") for row in content]
+    rows = len(content)
+    cols = len(content[0])
+
+    marker = {}
+    for i, row in enumerate(content):
+        for j, char in enumerate(row):
+            if char.isalnum() and not char in marker:
+                marker[char] = [(i, j)]
+            elif char.isalnum() and char in marker:
+                marker[char].append((i, j))
+    return marker, rows, cols
+
+
+antenas, rows, cols = process_input("input.txt")
+
+# Part 1
+
+
+def find_antinodes(antenas, rows, cols):
+    antinodes = set()
+    def dist(p1, p2): return (p2[0]-p1[0], p2[1]-p1[1])
+    def add(x, y): return (x[0] + y[0], x[1] + y[1])
+    def subtract(x, y): return (x[0] - y[0], x[1] - y[1])
+    for loc in antenas.values():
+        for i in range(len(loc)):
+            for j in range(i + 1, len(loc)):
+                p1, p2 = loc[i], loc[j]
+                d = dist(p1, p2)
+                ant1 = add(p2, d)
+                ant2 = subtract(p1, d)
+                if 0 <= ant1[0] < rows and 0 <= ant1[1] < cols:
+                    antinodes.add(ant1)
+                if 0 <= ant2[0] < rows and 0 <= ant2[1] < cols:
+                    antinodes.add(ant2)
+    return len(antinodes)
+
+
+print(find_antinodes(antenas, rows, cols))
+
+
+# Part 2
