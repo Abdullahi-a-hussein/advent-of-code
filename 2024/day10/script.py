0a40ea08c21f1a6114a6b05222b0327fd374de48
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2024-12-29 18:58:26 -0500

    day 10 of advent of code 2024 complete

--- 2024/day10/script.py
+++ 2024/day10/script.py
@@ -44,3 +44,29 @@ def bfs(grid, r, c):
 
 # Part 1
 print(sum(bfs(grid, r, c) for r, c in trailheads))
+
+# Part 2
+
+
+def rating(grid, r, c):
+    q = deque([(r, c)])
+    seen = {(r, c): 1}
+    total = 0
+    while len(q) > 0:
+        cr, cc = q.popleft()
+        if grid[cr][cc] == 9:
+            total += seen[(cr, cc)]
+        for nr, nc in [(cr-1, cc), (cr+1, cc), (cr, cc-1), (cr, cc+1)]:
+            if nr < 0 or nc < 0 or nr >= rows or nc >= cols:
+                continue
+            if grid[nr][nc] != grid[cr][cc] + 1:
+                continue
+            if grid[nr][nc] in seen:
+                seen[(nr, nc)] += 1
+                continue
+            seen[(nr, nc)] = 1
+            q.append((nr, nc))
+    return total
+
+
+print(sum(rating(grid, r, c) for r, c in trailheads))
